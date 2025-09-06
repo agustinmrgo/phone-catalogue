b2f16f3614c1f789ad131acc4af2b4e81af10aa7
agustinmrgo <agustinmrgo@gmail.com>
2025-09-06 21:59:28 +0200

    typescript fixes

--- api/openapi-ts.config.ts
+++ api/openapi-ts.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "@hey-api/openapi-ts";
 export default defineConfig({
   input: "./spec.yaml",
   output: "./generated",
-  plugins: ["@hey-api/typescript", "@hey-api/client-axios", "@hey-api/sdk"],
+  plugins: ["@hey-api/types", "@hey-api/services"],
 });
